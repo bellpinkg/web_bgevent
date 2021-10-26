0d80db336fe6b38446c8d3070b42007c46850abd
bellpinkg <3237128503@qq.com>
2021-10-26 19:01:16 +0800

    完成了登录和注册功能的开发

---
+++ assets/js/login.js
@@ -0,0 +1,63 @@
+$(function() {
+    $('#link-reg').on('click', function() {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    $('#link-login').on('click', function() {
+        $('.reg-box').hide();
+        $('.login-box').show()
+    });
+
+    // 从layui中获取form对象,layer对象
+    var form = layui.form;
+    var layer = layui.layer;
+    // 通过 form.verify() 函数自定义校验规则
+    form.verify({
+        // 自定义了pwd这个校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 自定义再次确认密码校验规则
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd != value) {
+                return '两次密码输入不一致'
+            }
+        }
+    })
+
+    // 监听注册表单提交事件
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault();
+        $.post('/api/reguser', {
+            username: $('.reg-box [name=username]').val(),
+            password: $('.reg-box [name=password]').val()
+        }, function(res) {
+            if (res.status != 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg(res.message + '请登录');
+            // 模拟人的点击行为
+            $('#link-login').click();
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/api/login',
+            // 快速获取表单中的数据
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status != 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功');
+                // console.log(res.token);
+                // 跳转到后台主页
+                location.href = '/index.html';
+            }
+        })
+    })
+
+})
