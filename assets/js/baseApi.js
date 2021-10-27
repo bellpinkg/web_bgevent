e0092bf7e9f2fed73be9fec0a2f770205ef65d5f
bellpinkg <3237128503@qq.com>
2021-10-27 15:22:50 +0800

    完成了主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,5 +3,25 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
-    console.log(options.url);
+
+    // 统一为有权限的接口设置请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 函数
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    options.complete = function(res) {
+        // console.log('执行了回调');
+        // console.log(res);  //查看res结果,可以使用 res.responseJSON 拿到服务器响应回来的数据
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token');
+            // 2.强制跳回登录页面
+            location.href = '/login.html'
+        }
+    }
 })
